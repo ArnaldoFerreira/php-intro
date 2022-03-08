ba19bb1a105fa93dee3bdffd199a22e110728cb7
Arnaldo <arnaldodsfjr@gmail.com>
2022-03-08 17:24:02 -0300

    adding form  field validation

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>FORMULARIO PARA INSCRICAO DE COMPETIDORES</p>
+    
+    <form action="script.php" method="post">
+        <p>Seu nome aqui: <input type="text" name="nome" /> </p>
+        <p>Sua idade: <input type="text" name="idade" /> </p>
+        <p><input type="submit" value="Enviar dados do computador"/> </p>
+    </form>
+</body>
+</html>
